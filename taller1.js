30696da8b6581a5585f9a2012f58e8fb282ac03d
Elkin Santis <93928718+ErSantis@users.noreply.github.com>
2025-02-07 18:33:50 -0500

    Add files via upload

---
+++ taller1.js
@@ -0,0 +1,48 @@
+//Punto 1
+const convertidorTemp = (grados) => {
+    let fahrenheit = (grados * 9 / 5) + 32;
+    return fahrenheit;
+}
+
+//Puunto 2
+const resolvedor = (a, b, c, flag) => {
+    let x1, x2;
+    let raiz = Math.pow(b, 2) - 4 * a * c;
+
+    x1 = (-b + Math.sqrt(raiz)) / (2 * a);
+    x2 = (-b - Math.sqrt(raiz)) / (2 * a);
+
+    if (flag) {
+        return x1;
+    } else {
+        return x2;
+    }
+
+}
+
+//Punto 3
+const mejorParidad = (num1) => {
+    let paridad = num1 % 2 == 0 ? true : false;
+    return paridad;
+}
+
+//Punto 4
+const peorParidad = (num) => {
+
+    let resultado = false;
+    let contador = 0;
+
+    while (contador < num) {
+        contador += 2;
+        if (contador === num) {
+            resultado = true;
+        }
+    }
+
+    return resultado;
+}
+
+console.log(convertidorTemp(30));
+console.log(resolvedor(1, 5, 4, false));
+console.log(mejorParidad(4));
+console.log(peorParidad(6));
